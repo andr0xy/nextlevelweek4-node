227335d97aa594e9cb106be9c4854963be3bc913
andr0xy <rossignolliandrei@gmail.com>
2021-02-25 10:12:17 -0300

    Class #2 - Node.js

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import 'reflect-metadata'
+import express from 'express'
+import './database'
+import { router } from './routes'
+
+const app = express()
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(router)
+
+app.listen(3001, () => console.log('Server -> running on port 3001.'))
